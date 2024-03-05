42c9880d202223d295cbd82b34d4540f19d7ca6b
Dominik Schlack <dominik.schlack@frequentis.com>
2024-03-05 14:44:13 +0100

    Rework Conifg

--- lua/mattern/init.lua
+++ lua/mattern/init.lua
@@ -1,16 +1,37 @@
-local config = require("mattern.config")
-local M = {}
+local Mattern = {}
+local H = {}
 
 local ns_mattern = vim.api.nvim_create_namespace("mattern")
 
-M.setup = config.setup
+Mattern.setup = function(config)
+    _G.Mattern = Mattern
+    config = H.setup_config(config)
+end
 
+Mattern.config = {
+    markers = {
+        -- Out Custom Marker Definitions
+        --  | Pattern      | Text to Write  | Color        | Optional Filetype |
+        { 'acs:', 'Matched ACS', "@comment.error", "lua" },
+    }
+}
 
-function M.mattern_print()
+H.default_config = vim.deepcopy(Mattern.config)
+
+H.setup_config = function(config)
+    -- General idea: if some table elements are not present in user-supplied
+    -- `config`, take them from default config
+    vim.validate({ config = { config, 'table', true } })
+    config = vim.tbl_deep_extend('force', vim.deepcopy(H.default_config), config or {})
+
+    return config
+end
+
+function Mattern.mattern_print()
     -- if vim.bo.filetype == "yaml" then
     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     for index, line in ipairs(lines) do
-        for _, pattern in ipairs(config.options.markers) do
+        for _, pattern in ipairs(Mattern.config.markers) do
             if (pattern[4] == nil) or (vim.bo.filetype == pattern[4]) then
                 if string.find(line, pattern[1]) then
                     vim.api.nvim_buf_set_extmark(0, ns_mattern, index - 1, 0,
@@ -26,4 +47,4 @@ function M.mattern_print()
     end
 end
 
-return M
+return Mattern
